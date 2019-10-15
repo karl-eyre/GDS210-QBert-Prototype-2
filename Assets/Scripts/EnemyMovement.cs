942f4853389190b8487280aed609c7def4f48cf5
Oscar Fallgren Glemme <1013978@student.sae.edu.au>
2019-10-15 13:57:43 +0800

    navmesh link

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public GameObject wayPoint;
+
+    private NavMeshAgent navAgent;
+
+    void Start()
+    {
+
+        navAgent = gameObject.GetComponent<NavMeshAgent>();
+        Debug.Log(navAgent.destination);
+
+    }
+
+    void Update()
+    {
+        
+        if(navAgent.destination != wayPoint.transform.position)
+        {
+            Debug.Log("Target");
+            navAgent.SetDestination(wayPoint.transform.position);
+
+        }
+
+    }
+}
