b6eeb4953b678a57e55312213ff1a86970627914
Karl Eyre <karl_eyre@hotmail.com>
2019-10-18 01:25:19 +1100

    Movement + Timer Added
    @Oscar, Load my scene and have a test when you load it up. 
    use numpad 1245 as directional for player movement,
    
    movement needs some bug fix. Will occasionally set sideways when on the bottom of the board and trying to press down.  
    
    I have also added a timer. The player will only move once at the end of every second.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent navAgent;
+
+    //position for player to move
+    public GameObject moveSqaure;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navAgent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //control input for player with numpad 1,2,4 & 5
+
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Debug.Log("Keypad1");
+            SetPosition(new Vector3(1, -1, 0));
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            Debug.Log("Keypad2");
+            SetPosition(new Vector3(0, -1, 1));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            Debug.Log("Keypad4");
+            SetPosition(new Vector3(0, 1, -1));
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            Debug.Log("Keypad5");
+            SetPosition(new Vector3(-1, 1, 0));
+            //moveSqaure.transform.position = this.gameObject.transform.position + new Vector3(-1, 1, 0);
+        }   
+    }
+
+    public void SetPosition(Vector3 x)
+    {
+        //update move location based on player position
+        moveSqaure.transform.position = this.gameObject.transform.position + (x);
+    }
+
+    //make movement happen each second of the game
+    public void TriggerSecond()
+    {
+     navAgent.SetDestination(moveSqaure.transform.position);
+    }
+   
+}
---
+++ Assets/Scripts/showTime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showTime : MonoBehaviour
+{
+    public int seconds;
+    float secondsToCovert = 0.0f;
+    private Text clockTimer;
+
+    private int triggerActionTimer = 0;
+    public GameObject player;
+
+    //Show the time per second as text
+    void Start()
+    {
+        clockTimer = this.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        secondsToCovert = Time.realtimeSinceStartup;
+        seconds =  Mathf.FloorToInt(secondsToCovert);
+        clockTimer.text = "Seconds: " + seconds.ToString();
+    
+        if (seconds > triggerActionTimer)
+        {
+            player.GetComponent<PlayerMovement>().TriggerSecond();
+            triggerActionTimer = seconds;
+            print(seconds);
+        }
+    }
+}
