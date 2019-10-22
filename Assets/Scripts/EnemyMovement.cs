297bc1c64f361873a1774dcd3f2eed85d66b74ef
Oscar <1013978@student.sae.edu.au>
2019-10-22 14:04:13 +0800

    enemy movment

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,26 +7,66 @@ public class EnemyMovement : MonoBehaviour
 {
 
     public GameObject wayPoint;
+    public GameObject pastLoc;
+    public float pastLocDist;
 
+    private float currentTime;
+    private int seconds;
     private NavMeshAgent navAgent;
+    private int moveTimer;
+    private GameObject[] waypoints;
+    private float playerDist;
 
     void Start()
     {
 
+        pastLoc = GameObject.FindGameObjectWithTag("PastLoc");
         navAgent = gameObject.GetComponent<NavMeshAgent>();
-        Debug.Log(navAgent.destination);
+        pastLoc.transform.position = transform.position;
+        //waypoints = GameObject.FindGameObjectsWithTag("WayPoint");
+        //FindWayPoints();
 
     }
 
     void Update()
     {
-        
-        if(navAgent.destination != wayPoint.transform.position)
+
+        currentTime = Time.realtimeSinceStartup;
+        seconds = Mathf.FloorToInt(currentTime);
+
+        pastLocDist = Vector3.Distance(pastLoc.transform.position, transform.position);
+
+        playerDist = Vector3.Distance(wayPoint.transform.position, transform.position);
+
+        if(pastLocDist > 1.4f)
         {
-            Debug.Log("Target");
-            navAgent.SetDestination(wayPoint.transform.position);
+            moveTimer = seconds += 1;
+            pastLoc.transform.position = transform.position;
+            navAgent.destination = pastLoc.transform.position;
 
         }
 
+        if(seconds > moveTimer)
+        {
+
+            navAgent.destination = wayPoint.transform.position;
+
+        }
+
+        /*if(playerDist > .5f)
+        {
+
+            FindWayPoints();
+
+        }*/
+
     }
+
+    /*void FindWayPoints()
+    {
+
+        wayPoint = waypoints[Random.Range(0, waypoints.Length)];
+
+    }*/
+
 }
