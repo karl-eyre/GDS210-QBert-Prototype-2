34e722732bbc10d164679f3213e05d9918846a24
Karl Eyre <karl_eyre@hotmail.com>
2019-10-31 22:58:47 +1100

    Infector Spin up added
    Works most of the time.

--- Assets/Scripts/infectorMovement.cs
+++ Assets/Scripts/infectorMovement.cs
@@ -8,6 +8,14 @@ public class infectorMovement : MonoBehaviour
     public float spinSpeed;
     public float spinMultiplyer = 0.1f;
 
+    public float spinUpMultiplyerAddition;
+
+    public GameObject timer;
+
+    public bool spinUpAndInfect = true;
+    public int timeToWaitBeforeSpinUp;
+    public int timeToSpin;
+
     void Start()
     {
         infectorColour = GetComponent<Renderer>();
@@ -15,7 +23,49 @@ public class infectorMovement : MonoBehaviour
 
     void Update()
     {
+        SpinUpOrDown();
+
+        if (spinMultiplyer >= 0.4f && spinUpAndInfect == true)
+        {
+            this.tag = "InfectorEnemy";
+        }
+
+        if (spinMultiplyer < 0.4f)
+        {
+            this.tag = "Untagged";
+        }
+
+        if (spinMultiplyer > 0.5f)
+        {
+            spinUpAndInfect = false; 
+        }
+
         spinSpeed = spinSpeed + spinMultiplyer;
         infectorColour.material.SetTextureOffset("_MainTex", new Vector2(spinSpeed, 0));
+
+        if (timer.GetComponent<showTime>().seconds > timeToWaitBeforeSpinUp)
+        {
+            spinUpAndInfect = true;
+            timeToWaitBeforeSpinUp = timer.GetComponent<showTime>().seconds + 20;
+        } 
     }
+
+    void SpinUpOrDown()
+    {
+        if (spinUpAndInfect == true && spinMultiplyer < 0.51f)
+        {
+            spinMultiplyer = spinMultiplyer + spinUpMultiplyerAddition;
+        }
+
+        if (spinUpAndInfect == false && spinMultiplyer > 0.0f)
+        {
+            spinMultiplyer = spinMultiplyer + (spinUpMultiplyerAddition * -1);
+        }
+    }
+
+    public void TurnOnInfector()
+    {
+        spinUpAndInfect = !spinUpAndInfect;
+    } 
+
 }
