7349cec56ac19804300c3a8a27eee73c61e61edb
Oscar <1013978@student.sae.edu.au>
2019-10-31 15:20:26 +1100

    game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject[] cubes;
+    void Start()
+    {
+
+        cubes = GameObject.FindGameObjectsWithTag("Cubes");
+
+    }
+
+    
+    void Update()
+    {
+        
+
+
+    }
+
+    public void WinCheck()
+    {
+
+        foreach (GameObject cube in cubes)
+        {
+
+
+
+        }
+
+    }
+
+}
