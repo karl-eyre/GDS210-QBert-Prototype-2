500eb3132fffa1d4c820fbcada1c0339f24c93bd
Karl Eyre <Karl_eyre@hotmail.com>
2019-10-31 12:59:26 +0800

    Win Condition Created
    Needs to be assigned

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-
-
+    public GameObject[] CubesColours;
+    public int Score;
 
     void Start()
     {
-
-        
-
+        CubesColours = GameObject.FindGameObjectsWithTag("Cube");
     }
 
     
     void Update()
     {
+        if (Score == 0)
+        {
+            print("YOU WIN THE GAME!!!");
+        }
     }
 
+    void CountScore()
+    {
+        int x = 0;
+
+        foreach (GameObject cube in CubesColours)
+        {
+            ColourChange Currentstate = cube.GetComponent<ColourChange>();
+            if(cube.GetComponent<MeshRenderer>().material != Currentstate.thirdMat)
+            {
+                x = x + 1;
+            }
+        }
+
+        Score = x;
+
+    }
 
 }
